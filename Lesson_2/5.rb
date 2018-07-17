f4dde1360cd482a65ba8cc7b7f1be5c3364836d8
Ivan S <i.shiryaev@protonmail.com>
2018-07-17 20:55:49 +0700

    Add a solution for task 5

---
+++ Lesson_2/5.rb
@@ -0,0 +1,27 @@
+def days_in_february(year)
+  if year % 400 == 0
+    29
+  elsif year % 4 == 0 && year % 100 != 0
+    29
+  else
+    28
+  end
+end
+
+puts "Введите число: "
+day = gets.to_i
+puts "Введите номер месяца: "
+month = gets.to_i
+puts "Введите год: "
+year = gets.to_i
+
+days_in_month = [31, days_in_february(year), 30, 31, 30, 31, 30, 31, 30, 31, 30] # Количество дней в декабре не имеет значения
+
+if month == 1
+  puts day
+else
+  days_in_month[0..month-1].each do |days|
+    day += days
+  end
+  puts day
+end
