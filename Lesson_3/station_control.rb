1113d7759de7f771a640810e806542b94d45d14d
Ivan S <i.shiryaev@protonmail.com>
2018-07-19 20:50:36 +0700

    Add fixes for Lesson 3 according to code review

--- Lesson_3/station_control.rb
+++ Lesson_3/station_control.rb
@@ -20,12 +20,18 @@ class Station
 end
 
 class Route
-  attr_reader :stations, :first_station, :last_station
+  attr_reader :stations
 
   def initialize(first_station, last_station)
-    @first_station = first_station
-    @last_station = last_station
     @stations = [first_station, last_station]
+  end
+
+  def first_station
+    stations.first
+  end
+
+  def last_station
+    stations.last
   end
 
   def add_station(station)
@@ -36,53 +42,70 @@ class Route
     allowed_stations = stations.slice(1...-1) # Так как стартовая и конечная точки маршрута не могут быть удалены.
     stations.delete(station) if allowed_stations.include?(station)
   end
+
+  def stations_list
+    stations
+  end
 end
 
 class Train
-  attr_accessor :speed, :carriages_amount, :current_station, :previous_station, :next_station, :route
-  attr_reader :type
+  attr_accessor :speed, :carriages_amount
+  attr_reader :type, :speed, :route, :current_station_index
 
   def initialize(number, type, carriages_amount)
     @number = number
     @type = type
     @carriages_amount = carriages_amount
+    @speed = 0
+  end
+
+  def current_station
+    route.stations[current_station_index]
+  end
+
+  def previous_station
+    route.stations[current_station_index - 1] unless current_station_index == 0
+  end
+
+  def next_station
+    route.stations[current_station_index + 1] unless current_station_index == route.stations.size - 1
+  end
+
+  def increase_speed(speed)
+    @speed += speed if speed > 0
   end
 
   def stop
-    self.speed = 0
+    @speed = 0
   end
 
   def add_carriage
-    self.carriages_amount += 1 if speed == 0
+    @carriages_amount += 1 if speed == 0
   end
 
   def remove_carriage
-    self.carriages_amount -= 1 if speed == 0 && carriages_amount > 0
+    @carriages_amount -= 1 if speed == 0 && carriages_amount > 0
   end
 
   def set_route(route)
     @route = route
-    @current_station = route.first_station
-    set_stations(current_station)
+    route.stations.first.accept_train(self)
+    @current_station_index = 0
   end
 
   def move_to_next_station
     unless next_station.nil?
-      self.current_station = next_station
-      set_stations(current_station)
+      route.stations[current_station_index].send_train(self)
+      @current_station_index += 1
+      route.stations[current_station_index].accept_train(self)
     end
   end
 
   def move_to_previous_station
     unless previous_station.nil?
-      self.current_station = previous_station
-      set_stations(current_station)
+      route.stations[current_station_index].send_train(self)
+      @current_station_index -= 1
+      route.stations[current_station_index].accept_train(self)
     end
-  end
-
-  def set_stations(current_station)
-    station_index = route.stations.index(current_station)
-    @previous_station = route.stations[station_index - 1] unless station_index == 0
-    @next_station = route.stations[station_index + 1] unless station_index == route.stations.size - 1
   end
 end
